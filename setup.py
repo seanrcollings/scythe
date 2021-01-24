03252ba9ae825cc21a08252a5e1b6ba1f5e4a8a7
Sean Collings <seanrcollings@gmail.com>
2021-01-23 19:13:32 -0700

    initial commit

---
+++ setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="scythe",
+    version="0.1",
+    license="MIT",
+    author="Sean Collings",
+    author_email="sean@seanrcollings.com",
+    description="Harvests are always better with a good tool",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_packages("src"),
+    package_dir={"": "src"},
+    zip_safe=False,
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.9",
+    # entry_points={"console_scripts": ["venture = venture.__main__:main"]},
+)
